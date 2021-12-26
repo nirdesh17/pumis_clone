9610c6b9c9b62169b00e8cce5d143b13e4fac62b
nirdesh17 <nirdeshdevadiya17@gmail.com>
2021-12-27 00:07:12 +0530

    profile tablayout added

---
+++ app/src/main/java/com/example/pumis_clone/FragmentAdapter.java
@@ -0,0 +1,32 @@
+package com.example.pumis_clone;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch (position)
+        {
+            case 1:
+                return new Ftab2();
+            case 2:
+                return new Ftab3();
+        }
+        return new Ftab1();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
--- app/src/main/java/com/example/pumis_clone/ProfileActivity.java
+++ app/src/main/java/com/example/pumis_clone/ProfileActivity.java
@@ -2,15 +2,60 @@ package com.example.pumis_clone;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
+public class ProfileActivity extends AppCompatActivity{
+
+
+
+    TabLayout tabLayout;
+    ViewPager2 pager2;
+    FragmentAdapter adapter;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        setTitle("Profile");
-    }
 
+        tabLayout=(TabLayout) findViewById(R.id.tabLayout1);
+        pager2=(ViewPager2) findViewById(R.id.vpager);
+
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fm,getLifecycle());
+        pager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.education));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.location));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.call));
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+    }
 }
